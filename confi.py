e65ac5e7a7185bc7a77fd010eac0baf8f9b2cacd
rentcubacar45-sketch <rentcubacar45@gmail.com>
2025-12-16 17:37:18 -0500

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,4 +1,5 @@
-Bot_token = "7872723290:AAEnpoT7WNPEC0ZIGrk9HHFRVcEKu2EbXRM"
+bot_token = "7872723290:AAEnpoT7WNPEC0ZIGrk9HHFRVcEKu2EbXRM"
 chanel_id =  -3566891542
 
 db_access = 2 #value int de la db, desmarcar line 1531 bot.py
+
